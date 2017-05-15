0766060af95af1f28b072e0d3e6984e5445720e4
Brian Tingle <brian.tingle.cdlib.org@gmail.com>
2017-05-14 22:37:52 -0700

    'future' (py2/3 compatiblity layer) is required

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='MediaJson',
     version='0.1.0',
     packages=['mediajson',],
-    install_requires=['boto3',],
+    install_requires=['boto3','future',],
     test_suite='nose.collector',
     tests_require=['nose',],
 )
